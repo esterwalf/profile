ca8948f9ddd299b7aa33fd5a5a959a7d85a5b620
Estelle Walford <66186219+esterwalf@users.noreply.github.com>
2024-05-30 19:50:29 +0100

    uploading profile

---
+++ script.js
@@ -0,0 +1,130 @@
+///////////////////////////
+/*INTRO TEXT*/
+let userNameQuestion = prompt("Hello, what is your name?");
+let userNameAnswer = document.createElement('h1');
+
+userNameAnswer.textContent = `${userNameQuestion}, Welcome to my Website`;
+
+document.body.appendChild(userNameAnswer);
+
+
+/*HOMEPAGE COLOUR CHANGER*/
+backgroundColourPurple = document.getElementById("backgroundPurple");
+function changeBackgroundToPurple(){
+    document.body.style.backgroundColor = "purple";
+}
+backgroundColourLavender = document.getElementById("backgroundLavender");
+function changeBackgroundToLavender(){
+    document.body.style.backgroundColor = "lavender";
+}
+fontColourPurple = document.getElementById("fontPurple");
+function changeFontToPurple() {
+	document.body.style.color = "purple";
+}
+fontColourLavender = document.getElementById("fontLavender");
+function changeFontToLavender(){
+    document.body.style.color = "lavender"
+}
+
+
+//using JS functions to change css
+//blue
+backgroundColourBlue = document.getElementById("backgroundBlue");
+function changeBackgroundToBlue(){
+	 document.body.style.backgroundColor = "blue";
+}
+backgroundColourCyan = document.getElementById("backgroundCyan");
+function changeBackgroundToCyan(){
+    document.body.style.backgroundColor = "cyan";
+}
+fontColourBlue = 
+	document.getElementById("fontBlue");
+function changeFontToBlue(){
+	document.body.style.color = "blue";
+}
+fontColourCyan = 
+document.getElementById("fontCyan");
+function changeFontToCyan(){
+    document.body.style.color = "cyan";
+}
+
+//red
+backgroundColourRed = document.getElementById("backgroundRed");
+function changeBackgroundToRed(){
+    document.body.style.backgroundColor = "red";
+}
+backgroundColourMaroon = document.getElementById("backgroundMaroon");
+function changeBackgroundToMaroon(){
+    document.body.style.backgroundColor = "maroon";
+}
+fontColourRed = document.getElementById("fontRed");
+function changeFontToRed(){
+    document.body.style.color = "red";
+}
+fontColourMaroon = document.getElementById("fontMaroon");
+function changeFontToMaroon(){
+    document.body.style.color = "maroon";
+}
+
+//green
+BackgroundColourGreen = document.getElementById("backgroundGreen");
+function changeBackgroundToGreen(){
+    document.body.style.backgroundColor = "green";
+}
+fontColourGreen = document.getElementById("fontGreen");
+function changeFontToGreen(){
+    document.body.style.color = "green";
+}
+
+//orange
+BackgroundColourOrange = document.getElementById("backgroundOrange");
+function changeBackgroundToOrange(){
+    document.body.style.backgroundColor = "orange";
+}
+fontColourOrange = document.getElementById("fontOrange");
+function changeFontToOrange(){
+    document.body.style.color = "orange";
+}
+
+//yellow
+BackgroundColourYellow = document.getElementById("backgroundYellow");
+function changeBackgroundToYellow(){
+    document.body.style.backgroundColor = "yellow";
+}
+FontColourYellow = document.getElementById("fontYellow")
+function changeFontToYellow(){
+    document.body.style.color = "yellow";
+}
+
+//pink
+BackgroundColourPink = document.getElementById("backgroundPink");
+function changeBackgroundToPink()
+{
+	document.body.style.backgroundColor = "pink";
+}
+FontColourPink = document.getElementById("fontPink");
+function ChangeFontToPink(){
+	document.body.style.color = "pink";
+}
+
+//white mode
+colourWhiteMode = document.getElementById("whiteMode");
+function changeToWhiteMode(){
+	document.body.style.backgroundColor = "white";
+	document.body.style.color = "black";
+}
+fontColourBlack = document.getElementById("fontBlack");
+function changeFontToBlack(){
+    document.body.style.color = "black";
+}
+
+//dark mode
+BackroundColourDarkMode = document.getElementById("DarkMode");
+function changeToDarkMode(){
+	document.body.style.backgroundColor = "black";
+	document.body.style.color = "white";
+}
+FontColourWhite = document.getElementById("fontWhite");
+function changeFontToWhite(){
+	document.body.style.color = "white";
+}
